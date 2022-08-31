625fb8b429c289816b5d65d29913affeca3bfbec
Oleksandr Naichenko <63473883+o-naichenko@users.noreply.github.com>
2022-08-31 11:46:35 +0300

    adds task-06 (input className change)

--- js/task-06.js
+++ js/task-06.js
@@ -1,22 +1,21 @@
 // Напиши скрипт, який би при втраті фокуса на інпут,
 // перевіряв його вміст на правильну кількість символів.
+// Скільки символів має бути в інпут, вказується в його атрибуті data-length.
+// Якщо введена відповідна кількість, то border інпут стає зеленим,
+// якщо неправильне - червоним.
+// Для додавання стилів, використовуй CSS-класи valid і invalid.
 
 const inputRef = document.getElementById('validation-input')
 const expectedInputLength = inputRef.getAttribute('data-length')
 let inputValue = ''
 
-const changeTextColor = (boolean, input) => {
-  const green = '#00FF00'
-  const red = '#FF0000'
-  input.style.color = boolean ? green : red
-}
-
 const checkInputLength = (inputValue, expectedLength) => {
-  changeTextColor(inputValue.length === parseInt(expectedLength), inputRef)
+  inputValue.length === parseInt(expectedInputLength)
+    ? (inputRef.className = 'valid')
+    : (inputRef.className = 'invalid')
 }
 
 inputRef.addEventListener('input', (e) => {
-  inputRef.style.color = '#000000'
   inputValue = e.target.value
 })
 
